f93a3a51c85a3a3c71f466aa6642210c6e59ef8e
StevenLove <lovestevend@gmail.com>
2015-10-10 19:35:17 -0400

    extracted murdlet logic

---
+++ src/murdlet.js
@@ -0,0 +1,88 @@
+var murdlet = function(){
+  var DB = require("./db.js"),
+    db = new DB(),
+    async = require('async');
+
+  // Local Murdlet Operations ///////////////////////////////
+
+  var CreateMurdlet = function(fid,uuid,time,comment){
+    var murdlet = {
+      fid:fid,
+      uuid:uuid,
+      time: time,
+      comment: comment
+    };
+    return murdlet;
+  }
+
+  var SubmitMurdlet = function(murdlet){
+    var fid = murdlet.fid;
+    // db.Store the murdlet
+    db.Store(murdlet.uuid, murdlet);
+    db.Load(fid,function(err, reply){
+      console.log("reply: " +reply)
+      var user = reply;
+      if(!user){
+        console.log("creating user");
+        user = CreateUser();
+      }
+      console.log("user: ");
+      console.log(user);
+      AddMurdletToUser(user,murdlet.uuid);
+      db.Store(fid,user);
+    });
+  }
+
+  var ListMurdlets = function(fid,callback){
+    db.Load(fid,function(err,reply){
+
+      var user = reply;
+      console.log("user: ");
+      console.log(user);
+      if(!user){
+        callback(null,[]);
+        return;
+      }
+      var uuids = user.murdlets;
+      var murdlets = [];
+      console.log("uuids: " + uuids);
+      async.forEachOf(
+        uuids,
+        function(uuid,key,async_cb){
+          console.log("uuid: " + uuid);
+          db.Load(uuid,function(err,reply){
+            console.log("reply: " + reply);
+            murdlets[key] = reply;
+            async_cb();
+          })
+        },
+        function(err){
+          if(err) console.error(" problem db.Loading murdlets to list them");
+          console.log(murdlets);
+          callback(null,murdlets);
+        }
+      );
+    });
+  }
+
+  // Boring Local Murdlet stuff //////////////////////////////////
+
+  var AddMurdletToUser = function(user,uuid){
+    user.murdlets.push(uuid);
+  }
+
+  var CreateUser = function(){
+    user = {
+      murdlets:[]
+    }
+    return user;
+  }
+
+  this.ListMurdlets = ListMurdlets;
+  this.SubmitMurdlet = SubmitMurdlet;
+  this.CreateMurdlet = CreateMurdlet;
+
+
+
+}
+module.exports = murdlet;
--- src/server.js
+++ src/server.js
@@ -1,9 +1,8 @@
 var express = require('express'),
     app = express(),
     path = require('path'),
-    async = require('async'),
-    DB = require('./db.js'),
-    db = new DB();
+    Murdlets = require('./murdlet.js'),
+    murdlets = new Murdlets();
     // imgur = require('imgur');
 
 
@@ -14,82 +13,6 @@ var _port = 8289;
 var _root_dir = "../"
 
 
-// Local Murdlet Operations ///////////////////////////////
-
-var CreateMurdlet = function(fid,uuid,time,comment){
-  var murdlet = {
-    fid:fid,
-    uuid:uuid,
-    time: time,
-    comment: comment
-  };
-  return murdlet;
-}
-
-var SubmitMurdlet = function(murdlet){
-  var fid = murdlet.fid;
-  // db.Store the murdlet
-  db.Store(murdlet.uuid, murdlet);
-  db.Load(fid,function(err, reply){
-    console.log("reply: " +reply)
-    var user = reply;
-    if(!user){
-      console.log("creating user");
-      user = CreateUser();
-    }
-    console.log("user: ");
-    console.log(user);
-    AddMurdletToUser(user,murdlet.uuid);
-    db.Store(fid,user);
-  });
-}
-
-var ListMurdlets = function(fid,callback){
-  db.Load(fid,function(err,reply){
-
-    var user = reply;
-    console.log("user: ");
-    console.log(user);
-    if(!user){
-      callback(null,[]);
-      return;
-    }
-    var uuids = user.murdlets;
-    var murdlets = [];
-    console.log("uuids: " + uuids);
-    async.forEachOf(
-      uuids,
-      function(uuid,key,async_cb){
-        console.log("uuid: " + uuid);
-        db.Load(uuid,function(err,reply){
-          console.log("reply: " + reply);
-          murdlets[key] = reply;
-          async_cb();
-        })
-      },
-      function(err){
-        if(err) console.error(" problem db.Loading murdlets to list them");
-        console.log(murdlets);
-        callback(null,murdlets);
-      }
-    );
-  });
-}
-
-// Boring Local Murdlet stuff //////////////////////////////////
-
-var AddMurdletToUser = function(user,uuid){
-  user.murdlets.push(uuid);
-}
-
-var CreateUser = function(){
-  user = {
-    murdlets:[]
-  }
-  return user;
-}
-
-
 
 // Hosting API for murdlets /////////////////////////////
 
@@ -97,7 +20,7 @@ var CreateUser = function(){
 var HostListMurdlets = function(fid){
   app.get("/list",function(req,res){
     var fid = req.query.fid;
-    ListMurdlets(fid,function(err,murdlets){
+    murdlets.ListMurdlets(fid,function(err,murdlets){
       res.end(JSON.stringify(murdlets));
     });
   })
@@ -109,8 +32,8 @@ var HostSubmitMurdlet = function(){
      var uuid = req.query.uuid;
      var time = req.query.time;
      var comment = req.query.comment;
-     var murdlet = CreateMurdlet(fid,uuid,time,comment);
-     SubmitMurdlet(murdlet);
+     var murdlet = murdlets.CreateMurdlet(fid,uuid,time,comment);
+     murdlets.SubmitMurdlet(murdlet);
      res.end("murdlet submitted");
   });
 }
