de70bccac90f5d4b419d5fa33c5faecca64243e8
gpwclark <gpwclark@gmail.com>
2015-10-10 20:08:16 +0000

    Working phonegap app that displays GPS coordinates, allows a user to recognize a comment, and takes a photo

--- mobile/comemurderme/platforms/android/assets/www/cordova_plugins.js
+++ mobile/comemurderme/platforms/android/assets/www/cordova_plugins.js
@@ -4,13 +4,21 @@ module.exports = [
         "file": "plugins/cordova-plugin-whitelist/whitelist.js",
         "id": "cordova-plugin-whitelist.whitelist",
         "runs": true
+    },
+    {
+        "file": "plugins/com.telerik.plugins.nativepagetransitions/www/NativePageTransitions.js",
+        "id": "com.telerik.plugins.nativepagetransitions.NativePageTransitions",
+        "clobbers": [
+            "window.plugins.nativepagetransitions"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.0.0",
-    "org.apache.cordova.geolocation": "0.3.12"
+    "org.apache.cordova.geolocation": "0.3.12",
+    "com.telerik.plugins.nativepagetransitions": "0.4.2"
 }
 // BOTTOM OF METADATA
 });
