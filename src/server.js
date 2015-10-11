047f7c88d692607e396adc215a25c9cc340d4aea
StevenLove <lovestevend@gmail.com>
2015-10-11 09:02:46 -0400

    1.0

--- src/server.js
+++ src/server.js
@@ -1,25 +1,25 @@
 var express = require('express'),
     app = express(),
-    path = require('path'),
+    path_lib = require('path'),
     Murdlets = require('./server-murdlets.js'),
     server_murdlets = new Murdlets(),
     imgur = require('imgur'),
-    bodyParser = require('body-parser'),
-    busboy = require('connect-busboy'),
-    multiparty = require('connect-multiparty');
-
-    app.use(busboy());
+    multiparty = require('connect-multiparty'),
+    base64_lib = require('node-base64-image');
+    // app.use(busboy());
 
 var _port = 8289;
 var _root_dir = "../"
 
-var UploadImage = function(base64){
+var UploadImage = function(base64, callback){
   imgur.uploadBase64(base64)
     .then(function(json){
-      console.log(json.data.link);
+      callback(null,json.data.link);
+      // console.log(json.data.link);
     })
     .catch(function(err){
       console.error(err.message);
+      callback(err,null);
     });
 }
 
@@ -47,7 +47,7 @@ var HostListMurdlets = function(){
 
 var HostCarousel = function(){
    app.get("/carousel",function(req,res){
-    var url = path.join(__dirname, _root_dir,"index.html")
+    var url = path_lib.join(__dirname, _root_dir,"index.html")
     res.sendFile(url);
   })
 }
@@ -58,7 +58,7 @@ var HostUI = function(){
     //   var html = GenerateHTML(murdlets);
     //   res.end(html);
     // });
-    var url = path.join(__dirname, _root_dir,"index.html")
+    var url = path_lib.join(__dirname, _root_dir,"index.html")
 
     res.sendFile(url);
   })
@@ -112,43 +112,62 @@ var GenerateHTML = function(murdlets){
 //     res.end(html);
 //   });
 // }
+// var path = "/tmp/KfCnASKVVX5jmlq-MUn8cfDm.jpg"
+
+
+var ConvertJPegToURL = function(path, callback){
+  ConvertToBase64(path,function(err, image){
+    if(err)console.error(err);
+    UploadImage(image, callback);
+  });
+}
+
+var ConvertToBase64 = function(path, callback){
+  var options = {localFile: true, string: true};
+   base64_lib.base64encoder(
+     path,
+     options,
+     callback
+   )
+}
 var HostUpload = function(){
   var multipartMiddleware = multiparty();
-
-   // var my_parser = bodyParser.raw(
-   //    {
-   //      // extended: false,
-   //      // limit: "2mb"
-   //    }
-   //  );  
+ 
    app.post("/images",multipartMiddleware,function(req,res){
      console.log(req.body); 
      console.log("POST");
      console.log(req.files);
+
+     var path = req.files.file.path;
+
+
+     console.log(req.body);
+     
+     var fid = req.body.userfbid;//"10205778030951425";
+     if(!fid) fid = "123";
+     var uuid = new Date().valueOf();
+     var datetime = req.body.userdatetime;//"1/1/1/1/";
+     if(!datetime) datetime= new Date().toLocaleString();
+     var comment=req.body.usercomment;//"HELLO THERE"
+     if(!comment) comment = "default comment";
+     var location = JSON.parse(req.body.userlocation);//"-57.123,13.13";
+     if(!location) location = JSON.stringify({"lat": "35.9025935", "lng":'-79.0745011'});
+     var latlng = location.lat + "," + location.lng;
+
+     ConvertJPegToURL(path, function(err, url){
+       if(err) console.error(err);
+       var murdlet = server_murdlets.CreateMurdlet(fid,uuid,datetime,comment,url,latlng)
+       server_murdlets.SubmitMurdlet(murdlet);
+     })
+
+
+
+     
      
      res.end("POST REQUEST");
     // var url = path.join(__dirname, _root_dir, "index.html")
     // res.sendFile(url);
   });
-  app.get("/images",function(req,res){
-     console.log(req.query);
-     console.log("GET");
-     res.end("GET");
-    // var url = path.join(__dirname, _root_dir, "index.html")
-    // res.sendFile(url);
-  });
-
-
-  // app.use(function(req, res) {
-  //   req.busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
-  //     // ... 
-  //   });
-  //   req.busboy.on('field', function(key, value, keyTruncated, valueTruncated) {
-  //     // ... 
-  //   });
-  //   req.pipe(req.busboy);
-  //   // etc ... 
-  // });
 
 }
 
@@ -160,14 +179,14 @@ var Listen = function(){
 
 var HostIndex = function(){
   app.get("/",function(req,res){
-    var url = path.join(__dirname, _root_dir, "index.html")
+    var url = path_lib.join(__dirname, _root_dir, "index.html")
     res.sendFile(url);
   });
 }
 
 var HostBundle = function(){
    app.get("/bundle.js",function(req,res){
-    var url = path.join(__dirname, _root_dir,"build/bundle.js")
+    var url = path_lib.join(__dirname, _root_dir,"build/bundle.js")
     res.sendFile(url);
   });
 }
